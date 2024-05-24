913294399a74290ef8793c59e0556d66d4ba9c85
H.Okpara <ST10454774@vcconnect.edu.za>
2024-05-24 14:18:38 +0200

    first commit

---
+++ app/src/main/java/com/st10454774/icetaskapplication/Income.kt
@@ -0,0 +1,11 @@
+package com.st10454774.icetaskapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Income : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_income)
+    }
+}
